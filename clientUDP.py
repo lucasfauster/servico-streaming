3394ec059457cafd4f2226e5c1dc3d4a954d7f56
Luam <luamsilva@id.uff.br>
2021-12-16 19:50:31 -0300

    Recebendo nome video e resolução

--- clientUDP.py
+++ clientUDP.py
@@ -1,3 +1,4 @@
+# This is client code to receive video frames over UDP
 import pickle
 
 import cv2, imutils, socket
@@ -13,7 +14,8 @@ host_ip = '127.0.1.1'
 print(host_ip)
 port = 5050
 
-def main ():
+
+def select_video_and_resolution():
     resAvaliable = ["240p", "480p", "720p"]
     print('Avaliable resolutions: 240p/ 480p/ 720p')
     resolution = input('Select one resolution: ')
@@ -21,35 +23,36 @@ def main ():
     if resolution not in resAvaliable:
         print('Resolution not found')
         return
+    resolution = resolution[:-1]
+    client_socket.sendto(pickle.dumps(['video.mkv', resolution]), (host_ip, port))
 
-    msg = pickle.dumps(['REPRODUZIR_VIDEO'])
-    client_socket.sendto(msg, ('127.0.1.1', 5050))
 
-    resposta, address = client_socket.recvfrom(1024)
-    if pickle.loads(resposta)[0] == 'ENVIAR RESOLUCAO':
-        print('chegou')
-        data = pickle.dumps(["VIDEO/RESOLUCAO", "video.mkv", resolution])
-        client_socket.sendto(data, address)
+def run_video():
+    while True:
         fps, st, frames_to_count, cnt = (0, 0, 20, 0)
-        while True:
-            packet, _ = client_socket.recvfrom(BUFF_SIZE)
-            data = base64.b64decode(packet, ' /')
-            npdata = np.fromstring(data, dtype=np.uint8)
-            frame = cv2.imdecode(npdata, 1)
-            frame = cv2.putText(frame, 'FPS: ' + str(fps), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-            cv2.imshow("RECEIVING VIDEO", frame)
-            key = cv2.waitKey(1) & 0xFF
-            if key == ord('q'):
-                client_socket.close()
-                break
-            if cnt == frames_to_count:
-                try:
-                    fps = round(frames_to_count / (time.time() - st))
-                    st = time.time()
-                    cnt = 0
-                except:
-                    pass
-            cnt += 1
+        packet, _ = client_socket.recvfrom(BUFF_SIZE)
+        data = base64.b64decode(packet, ' /')
+        npdata = np.fromstring(data, dtype=np.uint8)
+        frame = cv2.imdecode(npdata, 1)
+        frame = cv2.putText(frame, 'FPS: ' + str(fps), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.imshow("RECEIVING VIDEO", frame)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            client_socket.close()
+            break
+        if cnt == frames_to_count:
+            try:
+                fps = round(frames_to_count / (time.time() - st))
+                st = time.time()
+                cnt = 0
+            except:
+                pass
+        cnt += 1
+
+
+def main():
+    select_video_and_resolution()
+    run_video()
 
 if __name__ == "__main__":
     main()
