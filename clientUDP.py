2375009d47d961d72a4e3413943f90f5a768249f
Luam <luamsilva@id.uff.br>
2021-12-17 08:17:24 -0300

    Listagem de videos implementada

--- clientUDP.py
+++ clientUDP.py
@@ -1,10 +1,7 @@
-# This is client code to receive video frames over UDP
 import pickle
-import sys
-
-import cv2, imutils, socket
+import cv2
+import socket
 import numpy as np
-import time
 import base64
 
 BUFF_SIZE = 65536
@@ -13,6 +10,7 @@ client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
 host_name = socket.gethostname()
 host_ip = '127.0.1.1'
 port = 6000
+video_name=""
 
 
 def has_video():
@@ -26,16 +24,16 @@ def has_video():
 
 
 def select_video_and_resolution():
-    resAvaliable = ["240p", "480p", "720p"]
-    print('Avaliable resolutions: 240p/ 480p/ 720p')
-    resolution = input('Select one resolution: ')
+    global video_name
+    video_name = input("DIGITE O NOME DO VÍDEO: ")
+    res_avaliable = ["240p", "480p", "720p"]
+    resolution = input('DIGITE A RESOLUÇÃO: ')
 
-    if resolution not in resAvaliable:
-        print('Resolution not found')
+    if resolution not in res_avaliable:
+        print('RESOLUÇÃO NÃO DISPONÍVEL')
         return
 
-    msg = pickle.dumps(['REPRODUZIR_VIDEO', 'video.mkv', resolution])
-
+    msg = pickle.dumps(['REPRODUZIR_VIDEO', video_name, resolution])
     client_socket.sendto(msg, (host_ip, port))
 
 
@@ -44,24 +42,42 @@ def run_video():
     try:
         while True:
             packet, _ = client_socket.recvfrom(BUFF_SIZE)
-            print("Received {0} bytes of data.".format(sys.getsizeof(packet)))
+            # print("Received {0} bytes of data.".format(sys.getsizeof(packet)))
             data = base64.b64decode(packet, ' /')
             npdata = np.fromstring(data, dtype=np.uint8)
             frame = cv2.imdecode(npdata, 1)
-            cv2.imshow("RECEIVING VIDEO", frame)
+            cv2.imshow(video_name, frame)
             key = cv2.waitKey(1) & 0xFF
             if key == ord('q'):
                 client_socket.close()
+                print("VÍDEO FECHADO")
                 break
     except socket.timeout:
         print("VIDEO TERMINOU!")
 
 
+def list_videos():
+    msg = pickle.dumps(['LISTAR_VIDEOS'])
+    client_socket.sendto(msg, (host_ip, port))
+    resposta, _ = client_socket.recvfrom(BUFF_SIZE)
+    resposta = pickle.loads(resposta)
+    if type(resposta) is dict:
+        print("VÍDEOS DISPONÍVEIS: ")
+        for video in resposta:
+            print(f'{resposta[video][0]} - {resposta[video][1]}')
+
 
 def main():
-    select_video_and_resolution()
-    if has_video():
-        run_video()
+    opt = '0'
+    while opt != '3':
+        opt = input("\nESCOLHA UMA OPÇÃO:\n1) LISTAR VÍDEOS DISPONÍVEIS\n2) REPRODUZIR UM VÍDEO\n3) SAIR\n")
+        if opt == '1':
+            list_videos()
+        elif opt == '2':
+            select_video_and_resolution()
+            if has_video():
+                run_video()
+                break
 
 
 if __name__ == "__main__":
--- streaming_server/serverUDP.py
+++ streaming_server/serverUDP.py
@@ -5,7 +5,6 @@ import imutils
 import socket
 import base64
 
-
 BUFF_SIZE = 65536
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
@@ -14,49 +13,56 @@ host_ip = '127.0.1.1'
 port = 6000
 socket_address = (host_ip, port)
 server_socket.bind(socket_address)
-print('Listening at:', socket_address)
+print('ESCUTANDO EM:', socket_address)
+
+lista_videos = {'1': ['animacao.mp4', '240p'], '2': ['animacao.mp4', '480p'], '3': ['animacao.mp4', '720p'],
+                '4': ['pacman.mp4', '240p'], '5': ['pacman.mp4', '480p'], '6': ['pacman.mp4', '720p']}
 
 
 def find_video(video_name, resolution):
-	# noinspection PyArgumentList
-	video = cv2.VideoCapture(f'../videos/{resolution}/{video_name}')
-	return video
+    # noinspection PyArgumentList
+    video = cv2.VideoCapture(f'../videos/{resolution}/{video_name}')
+    return video
 
 
 def send_video(client_addr, video, resolution):
-	vid = find_video(video, resolution)
-	if vid.isOpened():
-		msg = f"REPRODUZINDO O VÍDEO {video}, COM RESOLUÇÃO {resolution}."
-		server_socket.sendto(pickle.dumps(msg), client_addr)
-		while vid.isOpened():
-			img, frame = vid.read()
-			if not img:
-				print(f'VIDEO FINISHED!')
-				break
-			frame = imutils.resize(frame, width=800)
-			encoded, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 80])
-			message = base64.b64encode(buffer)
-			server_socket.sendto(message, client_addr)
-		vid.release()
-	else:
-		msg = f"VIDEO {video} NÃO ENCONTRADO PARA RESOLUÇÃO {resolution}."
-		print(msg)
-		msg = pickle.dumps(msg)
-		server_socket.sendto(msg, client_addr)
+    vid = find_video(video, resolution)
+    if vid.isOpened():
+        print(f"ENVIANDO VÍDEO {video} COM RESOLUÇÃO {resolution} PARA {client_addr}")
+        msg = f"REPRODUZINDO O VÍDEO {video}, COM RESOLUÇÃO {resolution}."
+        server_socket.sendto(pickle.dumps(msg), client_addr)
+        while vid.isOpened():
+            img, frame = vid.read()
+            if not img:
+                print(f'ENVIO DO VÍDEO {video} COM RESOLUÇÃO {resolution} PARA {client_addr} TERMINADO!')
+                break
+            frame = imutils.resize(frame, width=600)
+            encoded, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 50])
+            message = base64.b64encode(buffer)
+            server_socket.sendto(message, client_addr)
+        vid.release()
+    else:
+        msg = f"VIDEO {video} NÃO ENCONTRADO PARA RESOLUÇÃO {resolution}."
+        print(msg)
+        msg = pickle.dumps(msg)
+        server_socket.sendto(msg, client_addr)
 
 
 def main():
-	while True:
-		msg, address = server_socket.recvfrom(BUFF_SIZE)
-		msg = pickle.loads(msg)
-		if type(msg) is list and msg[0] == 'REPRODUZIR_VIDEO':
-			video = msg[1]
-			resolution = msg[2]
-			thread = threading.Thread(target=send_video, args=(address, video, resolution))
-			thread.start()
-			print('GOT connection from ', address)
-			print("TOTAL CLIENTS ", threading.activeCount()-1)
+    while True:
+        msg, address = server_socket.recvfrom(BUFF_SIZE)
+        msg = pickle.loads(msg)
+        if type(msg) is list and msg[0] == 'REPRODUZIR_VIDEO':
+            video = msg[1]
+            resolution = msg[2]
+            thread = threading.Thread(target=send_video, args=(address, video, resolution))
+            thread.start()
+
+        elif type(msg) is list and msg[0] == 'LISTAR_VIDEOS':
+            msg = pickle.dumps(lista_videos)
+            server_socket.sendto(msg, address)
+            print("LISTA DE VIDEOS ENVIADA PARA", address)
 
 
 if __name__ == "__main__":
-	main()
+    main()
