080962d5482b6a31707e27a9efa0e29d73c8caa7
luamz <luamsilva@id.uff.br>
2022-03-23 00:03:39 -0300

    Change print to log clientTCP

--- client/clientTCP.py
+++ client/clientTCP.py
@@ -1,9 +1,12 @@
 import pickle
 import socket
+import logging
 
 
 class ClientTCP:
     def __init__(self):
+        logging.basicConfig(level=logging.INFO)
+        self.log = logging.getLogger("clientTCP")
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.host_ip = '127.0.1.1'
         self.port = 5000
@@ -11,10 +14,9 @@ class ClientTCP:
         self.client_socket.connect((self.host_ip, self.port))
         self.address_UDP = ()
 
-    @staticmethod
-    def has_permission(message):
+    def has_permission(self, message):
         if message == "PERMISSAO_NEGADA":
-            print("Apenas usuários Premium podem fazer essa ação!")
+            self.log.error("method = has_permission, message = Only premium users can do this action!")
             return False
         return True
 
@@ -26,13 +28,13 @@ class ClientTCP:
         message = ["ENTRAR_NA_APP", [name, user_type, self.address_UDP]]
         resp = self.send_recv_message(message)
         if resp[0] == "ENTRAR_NA_APP_ACK":
-            print("Você entrou no app")
+            self.log.info("method = log_in, message = User entered the app!")
             return True
-        print("Ocorreu um erro ao entrar no app")
+        self.log.error("method = log_in, message = An error occurred while entering the app!")
         return False
 
     def log_out(self):
-        print("TCHAU!")
+        self.log.info("method = log_out, message = User left the app! TCHAU!")
         self.send_recv_message(["SAIR_DA_APP"])
 
     def get_user_info(self):
@@ -45,35 +47,35 @@ class ClientTCP:
         resp = self.send_recv_message(["CRIAR_GRUPO"])
         if self.has_permission(resp[0]):
             if resp[0] == "CRIAR_GRUPO_ACK":
-                print("Grupo criado com sucesso!")
+                self.log.info("method = create_group, message = Group created successfully!")
             else:
-                print("Erro ao criar grupo")
+                self.log.error("method = create_group, message = An error occurred while creating group!")
 
     def add_to_group(self, chosen_user):
         resp = self.send_recv_message(["ADD_USUARIO_GRUPO", chosen_user])
         if self.has_permission(resp[0]):
             if resp[0] == "ADD_USUARIO_GRUPO_ACK":
-                print("Usuário adicionado ao grupo com sucesso!")
+                self.log.info("method = add_to_group, message = User added to group successfully!")
                 return True
             else:
-                print("Erro ao adicionar usuário ao grupo")
+                self.log.error("method = add_to_group, message = An error occurred while adding user to group!")
                 return False
 
     def get_group(self):
         resp = self.send_recv_message(["VER_GRUPO"])
         if resp[0] == "GRUPO_DE_STREAMING":
-            print("Usuários do grupo: {}".format(resp[1]))
+            self.log.info("method = get_group, message = Group users: {}".format(resp[1]))
             return resp[1]
         else:
-            print("Erro ao buscar grupo {}".format(resp[1]))
+            self.log.error("method = get_group, message = An error occurred while fetching group: {}".format(resp[1]))
             return None
 
     def remove_from_group(self, chosen_user):
         resp = self.send_recv_message(["REMOVER_USUARIO_GRUPO", chosen_user])
         if self.has_permission(resp[0]):
             if resp[0] == "REMOVER_USUARIO_GRUPO_ACK":
-                print("Usuário removido do grupo com sucesso!")
+                self.log.info("method = remove_from_group, message = User removed from group successfully!")
                 return True
             else:
-                print("Erro ao remover usuário do grupo")
+                self.log.error("method = add_to_group, message = An error occurred while removing user from group!")
                 return False
